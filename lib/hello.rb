1cf506fb27390eebd63f9285ebb1b17dc3e76532
Randy Steele <steelerx155@gmail.com>
2019-10-18 16:39:37 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,21 +1,5 @@
 def hello_t(names)
- i = 0
- 
- while i < names.lenght
- yield names[i]
- i = i + 1
- 
-end
-
-# call your method here!
+ # call your method here!
 end
 
 
-
-
-
-
-#tried this 
-names = ["Tim","Tom","Jim"].each do |name|
-   puts "#{name}"
-end
